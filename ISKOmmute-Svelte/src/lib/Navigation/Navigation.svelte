04e216ec0e332dcab2f589d7300b79cc268b4359
Calvin James Maximo <calvin.james.12391@gmail.com>
2024-03-06 18:24:45 +0800

    Added responsive sidebar

---
+++ ISKOmmute-Svelte/src/lib/Navigation/Navigation.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { getDrawerStore } from "@skeletonlabs/skeleton";
+
+    const drawerStore = getDrawerStore();
+
+    function drawerClose(){
+        drawerStore.close();
+    }
+</script>
+
+<nav class="list-nav p-4">
+	<ul>
+        <li><a href="./" on:click={drawerClose}>Home</a></li>
+		<li><a href="./settings" on:click={drawerClose}>Settings</a></li>
+		<li><a href="./tos" on:click={drawerClose}>Terms of Service</a></li>
+		<li><a href="./privacypolicy" on:click={drawerClose}>Privacy Policy</a></li>
+		<li><a href="./help" on:click={drawerClose}>Help</a></li>
+	</ul>
+</nav>
--- ISKOmmute-Svelte/src/routes/+layout.svelte
+++ ISKOmmute-Svelte/src/routes/+layout.svelte
@@ -1,15 +1,51 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, initializeStores, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
+	import Navigation from '$lib/Navigation/Navigation.svelte';
+
+	initializeStores();
+
+	const drawerStore = getDrawerStore();
+
+	function drawerOpen(): void {
+		drawerStore.open({});
+	}
+
+	function drawerClose(): void {
+		drawerStore.close();
+	}
 </script>
 
+<Drawer>
+	<div class="flex items-center">
+        <svg viewBox="0 0 50 80" class="fill-token w-4 h-4 cursor-pointer" on:click={drawerClose}>
+            <polygon points="45.63,75.8 0,40 45.63,4.2 50,8.6 9.37,40 50,71.4 "/>
+        </svg>
+		<h2 class="p-4">More Options</h2>
+    </div>
+	<hr />
+	<Navigation />
+	
+</Drawer>
+
 <!-- App Shell -->
-<AppShell>
+<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-32">
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<div class="flex items-center">
+					<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+						<span>
+							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+								<rect width="100" height="20" />
+								<rect y="30" width="100" height="20" />
+								<rect y="60" width="100" height="20" />
+							</svg>
+						</span>
+					</button>
+					<strong class="text-xl uppercase">ISKOmmute</strong>
+				</div>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
@@ -39,6 +75,12 @@
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
+
+	<!-- Left Sidebar Slot -->
+	<svelte:fragment slot="sidebarLeft">
+		<Navigation />
+	</svelte:fragment>
+
 	<!-- Page Route Content -->
 	<slot />
 </AppShell>
