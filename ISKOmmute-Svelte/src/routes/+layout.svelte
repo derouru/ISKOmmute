b72898cfcb9eb7ba48294219f8e54a80ae41370f
Calvin James Maximo <calvin.james.12391@gmail.com>
2024-03-06 20:45:01 +0800

    Added dropdown buttons for start and end loctions in src/routes/+page.svelte

--- ISKOmmute-Svelte/src/routes/+layout.svelte
+++ ISKOmmute-Svelte/src/routes/+layout.svelte
@@ -66,7 +66,7 @@
 				</a>
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
+					href="https://github.com/derouru/ISKOmmute"
 					target="_blank"
 					rel="noreferrer"
 				>
--- ISKOmmute-Svelte/src/routes/+page.svelte
+++ ISKOmmute-Svelte/src/routes/+page.svelte
@@ -1,5 +1,40 @@
-<div class="container p-10 space-y-4">
-	<h1>Homepage</h1>
-	<hr />
-	<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate nostrum, vero explicabo sunt ipsum asperiores. Molestias nostrum mollitia cumque veniam eum error atque, dolores nesciunt voluptas ea consectetur sed delectus.</p>
+<script>
+	let startPlaceHolder = 'Choose start point';
+	let endPlaceHolder = 'Choose end point';
+</script>
+
+<div class="container p-10 space-y-4 flex flex-col items-center h-screen">
+	<h1>Welcome to ISKOmmute!</h1>
+
+	<select class="select">
+		{#if startPlaceHolder}
+			<option value="" disabled selected>{startPlaceHolder}</option>
+		{/if}
+		<option value="AECH">Alumni Engineers Centennial Hall</option>
+		<option value="AngTBA">Ang Bahay ng Alumni</option>
+		<option value="A2">Area 2</option>
+		<option value="AIT">Asian Institute of Tourism</option>
+		<option value="Benitez">Benitez Hall</option>
+		<option value="Carillon">Carillon Tower</option>
+		<option value="CoTRL">Church of the Risen Lord</option>
+		<option value="Gyud">Gyud Food Hub</option>
+		<option value="IMath">Institute of Mathematics</option>
+	</select>
+
+	<select class="select">
+		{#if endPlaceHolder}
+			<option value="" disabled selected>{endPlaceHolder}</option>
+		{/if}
+		<option value="AECH">Alumni Engineers Centennial Hall</option>
+		<option value="AngTBA">Ang Bahay ng Alumni</option>
+		<option value="A2">Area 2</option>
+		<option value="AIT">Asian Institute of Tourism</option>
+		<option value="Benitez">Benitez Hall</option>
+		<option value="Carillon">Carillon Tower</option>
+		<option value="CoTRL">Church of the Risen Lord</option>
+		<option value="Gyud">Gyud Food Hub</option>
+		<option value="IMath">Institute of Mathematics</option>
+	</select>
+
+
 </div>
