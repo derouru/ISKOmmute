2cd50b3bdac75a1ca8bf96f5ba225a095a6cbe4d
Calvin James Maximo <calvin.james.12391@gmail.com>
2024-03-12 00:48:49 +0800

    Refactored src/+page.svelte to make it more readable

--- ISKOmmute-Svelte/src/routes/+page.svelte
+++ ISKOmmute-Svelte/src/routes/+page.svelte
@@ -5,49 +5,49 @@
 	let startPlace = '';
 	let endPlace = '';
 
+	let options = [
+        { value: "AECH", label: "Alumni Engineers Centennial Hall" },
+        { value: "AngTBA", label: "Ang Bahay ng Alumni" },
+        { value: "A2", label: "Area 2" },
+        { value: "AIT", label: "Asian Institute of Tourism" },
+        { value: "Benitez", label: "Benitez Hall" },
+        { value: "Carillon", label: "Carillon Tower" },
+        { value: "CoTRL", label: "Church of the Risen Lord" },
+        { value: "Gyud", label: "Gyud Food Hub" },
+        { value: "IMath", label: "Institute of Mathematics" }
+    ]
+
 	function handleClick() {
 		if (!startPlace || !endPlace) {
             window.alert('Please select both starting point and destination.');
-        } else {
+        } else if (startPlace === endPlace) {
+			window.alert('Starting point and destination cannot be the same.');
+		} else {
 			window.alert('Getting route from ' + startPlace + ' to ' + endPlace + '...');
 		}
 	}
 </script>
 
-<div class="container p-10 space-y-4 flex flex-col items-center h-screen">
-	<h1>Welcome to ISKOmmute!</h1>
+<div class="container p-10 space-y-4 flex flex-col items-center justify-center h-screen">
+	<h1>Where do you want to go?</h1>
 
 	<select class="select" bind:value={startPlace}>
 		{#if startPlaceHolder}
-			<option value="" disabled selected>{startPlaceHolder}</option>
-		{/if}
-		<option value="AECH">Alumni Engineers Centennial Hall</option>
-		<option value="AngTBA">Ang Bahay ng Alumni</option>
-		<option value="A2">Area 2</option>
-		<option value="AIT">Asian Institute of Tourism</option>
-		<option value="Benitez">Benitez Hall</option>
-		<option value="Carillon">Carillon Tower</option>
-		<option value="CoTRL">Church of the Risen Lord</option>
-		<option value="Gyud">Gyud Food Hub</option>
-		<option value="IMath">Institute of Mathematics</option>
+        	<option value="" disabled selected>{startPlaceHolder}</option>
+    	{/if}
+		{#each options as option (option.value)}
+			<option value={option.value}>{option.label}</option>
+		{/each}
 	</select>
 
 	<select class="select" bind:value={endPlace}>
 		{#if endPlaceHolder}
-			<option value="" disabled selected>{endPlaceHolder}</option>
-		{/if}
-		<option value="AECH">Alumni Engineers Centennial Hall</option>
-		<option value="AngTBA">Ang Bahay ng Alumni</option>
-		<option value="A2">Area 2</option>
-		<option value="AIT">Asian Institute of Tourism</option>
-		<option value="Benitez">Benitez Hall</option>
-		<option value="Carillon">Carillon Tower</option>
-		<option value="CoTRL">Church of the Risen Lord</option>
-		<option value="Gyud">Gyud Food Hub</option>
-		<option value="IMath">Institute of Mathematics</option>
+        	<option value="" disabled selected>{endPlaceHolder}</option>
+    	{/if}
+		{#each options as option (option.value)}
+			<option value={option.value}>{option.label}</option>
+		{/each}
 	</select>
 
 	<button class="btn btn-xl variant-filled" on:click={handleClick}>Generate Route</button>
-
-
 </div>
