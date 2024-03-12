8ec7d91d61f5779b0978475b4746e83a30c446bb
Calvin James Maximo <calvin.james.12391@gmail.com>
2024-03-12 21:45:05 +0800

    Added placeholder pages for other routes

---
+++ ISKOmmute-Svelte/src/routes/privacypolicy/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	/* insert relevant script here in the future */
+</script>
+
+<div class="container p-10 space-y-4 flex flex-col items-center justify-center h-screen">
+	<h1>Privacy Policy</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
