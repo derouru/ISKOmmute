12353a79784a66f07d834e70409aa02368a821ab
Calvin James Maximo <calvin.james.12391@gmail.com>
2024-03-12 17:08:56 +0800

    feat: Added initial splash screen

--- ISKOmmute-Svelte/src/routes/+layout.svelte
+++ ISKOmmute-Svelte/src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { AppShell, AppBar, initializeStores, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
+	import { onMount, onDestroy } from 'svelte';
 	import Navigation from '$lib/Navigation/Navigation.svelte';
+	import { writable } from 'svelte/store';
 
 	initializeStores();
 
@@ -14,51 +16,76 @@
 	function drawerClose(): void {
 		drawerStore.close();
 	}
+
+	// Create a store for the splash screen state
+	export const splashScreenStore = writable(true);
+
+	// Subscribe to the store and update the local state when the store changes
+	let showSplash;
+	const unsubscribe = splashScreenStore.subscribe(value => {
+		showSplash = value;
+	});
+
+	onMount(async () => {
+		setTimeout(() => {
+			splashScreenStore.set(false);
+		}, 3000);
+	});
+
+	onDestroy(() => {
+		unsubscribe();
+	});
 </script>
 
-<Drawer>
-	<div class="flex items-center">
-        <svg viewBox="0 0 50 80" class="fill-token w-4 h-4 cursor-pointer" on:click={drawerClose}>
-            <polygon points="45.63,75.8 0,40 45.63,4.2 50,8.6 9.37,40 50,71.4 "/>
-        </svg>
-		<h2 class="p-4">More Options</h2>
+{#if showSplash}
+	<!-- Splash screen -->
+    <div class="splash-screen">
+        <h1>Loading...</h1>
     </div>
-	<hr />
-	<Navigation />
-	
-</Drawer>
-
-<!-- App Shell -->
-<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-32">
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<div class="flex items-center">
-					<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
-						<span>
-							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
-								<rect width="100" height="20" />
-								<rect y="30" width="100" height="20" />
-								<rect y="60" width="100" height="20" />
-							</svg>
-						</span>
-					</button>
-					<strong class="text-xl uppercase">ISKOmmute</strong>
-				</div>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-
-	<!-- Left Sidebar Slot -->
-	<svelte:fragment slot="sidebarLeft">
+{:else}
+	<Drawer>
+		<div class="flex items-center">
+			<svg viewBox="0 0 50 80" class="fill-token w-4 h-4 cursor-pointer" on:click={drawerClose}>
+				<polygon points="45.63,75.8 0,40 45.63,4.2 50,8.6 9.37,40 50,71.4 "/>
+			</svg>
+			<h2 class="p-4">More Options</h2>
+		</div>
+		<hr />
 		<Navigation />
-	</svelte:fragment>
+		
+	</Drawer>
 
-	<!-- Page Route Content -->
-	<slot />
-</AppShell>
+	<!-- App Shell -->
+	<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-32">
+		<svelte:fragment slot="header">
+			<!-- App Bar -->
+			<AppBar>
+				<svelte:fragment slot="lead">
+					<div class="flex items-center">
+						<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+							<span>
+								<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+									<rect width="100" height="20" />
+									<rect y="30" width="100" height="20" />
+									<rect y="60" width="100" height="20" />
+								</svg>
+							</span>
+						</button>
+						<strong class="text-xl uppercase">ISKOmmute</strong>
+					</div>
+				</svelte:fragment>
+			</AppBar>
+		</svelte:fragment>
 
+		<!-- Left Sidebar Slot -->
+		<svelte:fragment slot="sidebarLeft">
+			<Navigation />
+		</svelte:fragment>
+
+		<!-- Page Route Content -->
+		<slot />
+	</AppShell>
+{/if}
 <style>
     :global(.skeleton-drawer) {
         height: 100vh;
