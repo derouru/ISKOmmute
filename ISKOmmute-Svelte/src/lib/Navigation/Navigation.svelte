f390ed86e9cb9e95eaaffe61080fba5050b6cf11
Calvin James Maximo <calvin.james.12391@gmail.com>
2024-03-12 23:53:03 +0800

    Fixed the navbar highlighting the wrong active page

--- ISKOmmute-Svelte/src/lib/Navigation/Navigation.svelte
+++ ISKOmmute-Svelte/src/lib/Navigation/Navigation.svelte
@@ -22,10 +22,10 @@
 
 <nav class="list-nav p-4">
     <ul>
-        <li><a href="/" on:click={drawerClose} class:active={$page.url.pathname === '/'} bind:this={links['/']}>Home</a></li>
-        <li><a href="/settings" on:click={drawerClose} class:active={$page.url.pathname === '/settings'} bind:this={links['/settings']}>Settings</a></li>
-        <li><a href="/tos" on:click={drawerClose} class:active={$page.url.pathname === '/tos'} bind:this={links['/tos']}>Terms of Service</a></li>
-        <li><a href="/privacypolicy" on:click={drawerClose} class:active={$page.url.pathname === '/privacypolicy'} bind:this={links['/privacypolicy']}>Privacy Policy</a></li>
-        <li><a href="/help" on:click={drawerClose} class:active={$page.url.pathname === '/help'} bind:this={links['/help']}>Help</a></li>
+        <li><a href="/" on:click={drawerClose} bind:this={links['/']}>Home</a></li>
+        <li><a href="/settings" on:click={drawerClose} bind:this={links['/settings']}>Settings</a></li>
+        <li><a href="/tos" on:click={drawerClose} bind:this={links['/tos']}>Terms of Service</a></li>
+        <li><a href="/privacypolicy" on:click={drawerClose} bind:this={links['/privacypolicy']}>Privacy Policy</a></li>
+        <li><a href="/help" on:click={drawerClose} bind:this={links['/help']}>Help</a></li>
     </ul>
 </nav>
