e33a97a220b320d104530326604c62f62bd6e92e
Calvin James Maximo <calvin.james.12391@gmail.com>
2024-03-13 10:24:52 +0800

    Added transition out of splash screen

--- ISKOmmute-Svelte/src/routes/+layout.svelte
+++ ISKOmmute-Svelte/src/routes/+layout.svelte
@@ -4,7 +4,7 @@
     import { onMount, onDestroy } from 'svelte';
     import Navigation from '$lib/Navigation/Navigation.svelte';
     import { splashScreenStore } from '$lib/stores';
-    import { fly } from 'svelte/transition';
+    import { fly, fade } from 'svelte/transition';
 
     // Exported data
     export let data;
@@ -32,7 +32,7 @@
     onMount(async () => {
         setTimeout(() => {
             splashScreenStore.set(false);
-        }, 3000);
+        }, 1000);
     });
 
     onDestroy(() => {
@@ -43,60 +43,62 @@
 {#if showSplash}
 	<!-- Splash screen -->
     <div class="splash-screen" style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-		<h1>Loading...</h1>
+		<h1>Welcome to ISKOmmute!</h1>
 	</div>
 {:else}
-	<Drawer>
-		<div class="flex items-center p-4"> 
-			<button class="fill-token w-3 h-3 cursor-pointer mr-4" on:click={drawerClose} aria-label="Close Drawer">
-				<svg viewBox="0 0 50 80">
-					<polygon points="45.63,75.8 0,40 45.63,4.2 50,8.6 9.37,40 50,71.4 "/>
-				</svg>
-			</button>
-			<h2>More Options</h2>
-		</div>
-		<hr />
-		<Navigation />
-		
-	</Drawer>
-
-	<!-- App Shell -->
-	<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-32">
-		<svelte:fragment slot="header">
-			<!-- App Bar -->
-			<AppBar>
-				<svelte:fragment slot="lead">
-					<div class="flex items-center">
-						<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
-							<span>
-								<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
-									<rect width="100" height="20" />
-									<rect y="30" width="100" height="20" />
-									<rect y="60" width="100" height="20" />
-								</svg>
-							</span>
-						</button>
-						<strong class="text-xl uppercase">ISKOmmute</strong>
-					</div>
-				</svelte:fragment>
-			</AppBar>
-		</svelte:fragment>
-
-		<!-- Left Sidebar Slot -->
-		<svelte:fragment slot="sidebarLeft">
-			<Navigation />
-		</svelte:fragment>
-
-		<!-- Page Route Content -->
-		{#key data.url}
-			<div 
-				in:fly={{ x: -200, duration: 400, delay: 400}}
-				out:fly={{ x: 200, duration: 400}}
-			>
-				<slot />
+	<div in:fade={{ duration: 400 }}>
+		<Drawer>
+			<div class="flex items-center p-4"> 
+				<button class="fill-token w-3 h-3 cursor-pointer mr-4" on:click={drawerClose} aria-label="Close Drawer">
+					<svg viewBox="0 0 50 80">
+						<polygon points="45.63,75.8 0,40 45.63,4.2 50,8.6 9.37,40 50,71.4 "/>
+					</svg>
+				</button>
+				<h2>More Options</h2>
 			</div>
-		{/key}
-	</AppShell>
+			<hr />
+			<Navigation />
+			
+		</Drawer>
+
+		<!-- App Shell -->
+		<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-32">
+			<svelte:fragment slot="header">
+				<!-- App Bar -->
+				<AppBar>
+					<svelte:fragment slot="lead">
+						<div class="flex items-center">
+							<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+								<span>
+									<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+										<rect width="100" height="20" />
+										<rect y="30" width="100" height="20" />
+										<rect y="60" width="100" height="20" />
+									</svg>
+								</span>
+							</button>
+							<strong class="text-xl uppercase">ISKOmmute</strong>
+						</div>
+					</svelte:fragment>
+				</AppBar>
+			</svelte:fragment>
+
+			<!-- Left Sidebar Slot -->
+			<svelte:fragment slot="sidebarLeft">
+				<Navigation />
+			</svelte:fragment>
+
+			<!-- Page Route Content -->
+			{#key data.url}
+				<div 
+					in:fly={{ x: -200, duration: 400, delay: 400}}
+					out:fly={{ x: 200, duration: 400}}
+				>
+					<slot />
+				</div>
+			{/key}
+		</AppShell>
+	</div>
 {/if}
 
 <style>
