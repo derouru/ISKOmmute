c541972c40005d44d55dcfc2f4fc4274a6eb8552
Calvin James Maximo <calvin.james.12391@gmail.com>
2024-05-09 21:59:21 +0800

    style: add styling to header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -62,18 +62,18 @@
 		</Drawer>
 
 		<!-- App Shell -->
-		<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-32">
+		<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-32" slotHeader="text-white">
 			<svelte:fragment slot="header">
 				<!-- App Bar -->
-				<AppBar>
+				<AppBar background="bg-[#9C293E]">
 					<svelte:fragment slot="lead">
 						<div class="flex items-center">
 							<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
 								<span>
-									<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
-										<rect width="100" height="20" class="hamburger"/>
-										<rect y="30" width="100" height="20" class="hamburger"/>
-										<rect y="60" width="100" height="20" class="hamburger"/>
+									<svg viewBox="0 0 100 80" class="fill-white w-4 h-4">
+										<rect width="100" height="20"/>
+										<rect y="30" width="100" height="20"/>
+										<rect y="60" width="100" height="20"/>
 									</svg>
 								</span>
 							</button>
@@ -128,9 +128,5 @@
 
 	.slot-content {
 		transform: translateY(-10%);
-	}
-
-	.hamburger {
-		fill: #979797;
 	}
 </style>
